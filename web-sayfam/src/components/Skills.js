49d0dc1977138a4da6a102fa52a86db2e708bd5a
emracatal <134175723+emracatal@users.noreply.github.com>
2023-09-10 20:42:34 +0300

    skills ongoing

--- web-sayfam/src/components/Skills.js
+++ web-sayfam/src/components/Skills.js
@@ -1,5 +1,18 @@
 import React from "react";
+import jslogo1 from "../assets/jslogo1.png";
+import Rectangle35 from "../assets/Rectangle35.png";
+import Rectangle33 from "../assets/Rectangle33.png";
 
 export default function Skills() {
-  return <div>Skills</div>;
+  return (
+    <>
+      <p className="text-purple text-5xl ml-72 mr-48 mt-12">Skills</p>
+      <img className="" src={jslogo1} alt="jslogo1"></img>
+      <p className="text-purple">JAVASCRIPT</p>
+      <img className="" src={Rectangle35} alt="Rectangle35"></img>
+      <p className="text-purple">JAVASCRIPT</p>
+      <img className="" src={Rectangle33} alt="Rectangle33"></img>
+      <p className="text-purple">JAVASCRIPT</p>
+    </>
+  );
 }
