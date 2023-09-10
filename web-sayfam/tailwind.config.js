cad6566e15cc2de05a90267f1e380c8b7a534b4c
emracatal <134175723+emracatal@users.noreply.github.com>
2023-09-10 17:08:01 +0300

    installed and tailwind works

---
+++ web-sayfam/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    colors: {
+      purple: "#4731D3;",
+      white: "#FFF;",
+      green: "#CBF281;",
+      black: "rgba(119, 119, 119, 1)",
+      "black-dark": "rgba(18, 11, 57, 1)",
+    },
+  },
+  plugins: [],
+};
