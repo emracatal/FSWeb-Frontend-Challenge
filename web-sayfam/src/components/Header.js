4daac449c5d4095de3663016ed8796bd166533b6
emracatal <134175723+emracatal@users.noreply.github.com>
2023-09-10 20:17:15 +0300

    header and hero in progress

--- web-sayfam/src/components/Header.js
+++ web-sayfam/src/components/Header.js
@@ -8,24 +8,25 @@ export default function Header() {
     <>
       <div className="header flex">
         <div className="header-left bg-purple min-w-[70%]">
-          <p className="text-green text-right">TÜRKÇEYE GEÇ</p>
-          <p className="text-green ml-52">EMRA</p>
+          <p className="text-green text-xl text-right">TÜRKÇEYE GEÇ</p>
+          <p className="text-green text-3xl ml-72">EMRA</p>
         </div>
         <div className="header-right bg-green min-w-[30%]">
-          <p className="text-purple text-left">DARK MODE</p>
+          <p className="text-purple text-xl text-left">DARK MODE</p>
         </div>
       </div>
 
-      <div className="hero-content min-h-screen flex">
-        <div className="hero-left bg-purple min-w-[70%]">
-          <p className="text-green text-3xl ml-52">
-            I am a Frontend Developer...
+      <div className="hero-content flex">
+        <div className="hero-left bg-purple min-w-[70%] self-end ">
+          <p className="text-green text-6xl ml-72 mr-48 mt-24">
+            I am a Frontend
           </p>
-          <p className="text-white ml-52">
+          <p className="text-green text-6xl ml-72 mr-48">Developer...</p>
+          <p className="text-white text-2xl ml-72 mr-48 mt-12">
             ...who likes to craft solid and scalable frontend products whith
             great user experiences
           </p>
-          <div className="buttons flex gap-2 ml-52">
+          <div className="buttons flex gap-2 ml-72 mt-12 mb-24 mr-48">
             <button className="bg-white rounded flex">
               <img src={github} alt="github"></img>
               <p className="text-purple">Github</p>
